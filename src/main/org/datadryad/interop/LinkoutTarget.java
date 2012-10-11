29bd1f273a8b2fa58de81c9e8e9b39962c85abf4
Peter Midford <peter.midford@gmail.com>
2012-10-11 19:18:53 -0400

    Add last piece so usable records are generated; dump XOM due to verifier problems; keep working NCBI verifier issues

--- src/main/org/datadryad/interop/LinkoutTarget.java
+++ src/main/org/datadryad/interop/LinkoutTarget.java
@@ -7,20 +7,13 @@
  */
 package org.datadryad.interop;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
-
-import nu.xom.Builder;
-import nu.xom.Comment;
-import nu.xom.Document;
-import nu.xom.Element;
-import nu.xom.Serializer;
-import nu.xom.Text;
 
 
 public class LinkoutTarget {
@@ -29,10 +22,14 @@ public class LinkoutTarget {
     
     TargetType myType;
     
+    final static int INDENTSIZE = 3;
     
-    final String ICONURL = "http://www.nescent.org/wg/dryad/images/7/7f/DryadLogo-Button.png";
-    final String DRYADBASE = "http://datadryad.org/discover?";
-    final String DRYADRULE = "query=&lo.doi;";
+    final static String ICONURL = "http://www.nescent.org/wg/dryad/images/7/7f/DryadLogo-Button.png";
+    final static String DRYADBASE = "http://datadryad.org/discover?";
+    final static String DRYADRULE = "query=" + "&lo.doi;";
+    final static String DTDROOTELEMENT = "LinkSet";
+    final static String DTDPUBLICID = "-//NLM//DTD LinkOut 1.0//EN";
+    final static String DTDURL = "http://www.ncbi.nlm.nih.gov/entrez/linkout/doc/LinkOut.dtd";
     
     
     final Set <DryadPackage> packages = new HashSet<DryadPackage>();
@@ -47,115 +44,186 @@ public class LinkoutTarget {
         
     }
     
+    
+    //NCBI seems to require query=&lo.doi;" which isn't really legal xml, so xom is out...
     void save(String targetFile) throws IOException{
-        Element root = generateLinkSet();
-        Document targetDocument = new Document(root);
-        
         FileOutputStream s = new FileOutputStream(targetFile, false);
-        Serializer serializer = new Serializer(s);
-        serializer.setIndent(3);
-        serializer.setMaxLength(72);
-        serializer.write(targetDocument);
+        OutputStreamWriter w = new OutputStreamWriter(s);
+        w.append("<?xml version=" + '"' + "1.0" + '"' + "encoding=" + '"' + "UTF-8" + '"' + "?>\n");
+        w.append("<!DOCTYPE " + DTDROOTELEMENT + " PUBLIC ");
+        w.append('"' + DTDPUBLICID + '"');
+        w.append(" " + '"' + DTDURL + '"' + ">\n");
+        String root = generateLinkSet();
+        w.append(root);
+        w.close();
     }
     
-    Element generateLinkSet(){
-        Element result = new Element("linkSet");
+    private String generateLinkSet(){
+        StringBuilder result = new StringBuilder(500*packages.size());
+        result.append("<" + DTDROOTELEMENT + ">\n");
         for (DryadPackage pkg : packages){
-            result.appendChild(generateComment(pkg));
-            result.appendChild(generateLink(pkg));
+            if (pkg.getPubDOI().length()>0){  //this is temporary, until we can lookup things in pubmed from metadata
+                if (!pkg.getPub().getPMIDs().isEmpty()){  //this should be caught upstream somewhere...
+                    result.append(generateComment(pkg));
+                    result.append(generateLink(pkg));
+                }
+            }
         }
-        return result;
+        result.append("</" + DTDROOTELEMENT + ">\n");       
+        return result.toString();
     }
     
-    Comment generateComment(DryadPackage pkg){
-        return new Comment("Link for publication: " + pkg.getPubDOI());
+    private String generateComment(DryadPackage pkg){
+        StringBuilder result = new StringBuilder(100);
+        result.append(getIndent(1));
+        result.append("<!-- Link for publication: ");
+        result.append(pkg.getPubDOI());
+        result.append("-->\n");
+        return result.toString();
     }
     
-    Element generateLink(DryadPackage pkg){
-        Element result = new Element("link");
-        result.appendChild(generateLinkId());
-        result.appendChild(generateProviderId());
-        result.appendChild(generateIconUrl());
-        result.appendChild(generateObjectSelector(pkg));
-        result.appendChild(generateObjectUrl(pkg));
-        return result;
+    private String generateLink(DryadPackage pkg){
+        StringBuilder result = new StringBuilder(500);
+        result.append(getIndent(1));        
+        result.append("<Link>\n");
+        result.append(generateLinkId());
+        result.append(generateProviderId());
+        result.append(generateIconUrl());
+        result.append(generateObjectSelector(pkg));
+        result.append(generateObjectUrl(pkg));
+        result.append(getIndent(1));        
+        result.append("</Link>\n");
+        return result.toString();
     }
     
     
-    Element generateLinkId(){
-        Element result = new Element("LinkId");
+    private String generateLinkId(){
+        StringBuilder result = new StringBuilder(60);
+        result.append(getIndent(2));        
+        result.append("<LinkId>");
         if (myType == TargetType.PUBLINKS)
-            result.appendChild("dryad.pubmed");
+            result.append("dryad.pubmed.");
         else
-            result.appendChild("dryad.seq");
-        return result;
+            result.append("dryad.seq.");
+        result.append(".yyyy-mm-dd");
+        result.append("</LinkId>\n");
+        return result.toString();
     }
     
-    Element generateProviderId(){
-        Element result = new Element("ProviderId");
-        result.appendChild("7893");
-        return result;
+    private String generateProviderId(){
+        StringBuilder result = new StringBuilder(60);
+        result.append(getIndent(2));
+        result.append("<ProviderId>");
+        result.append("7893");
+        result.append("</ProviderId>\n");
+        return result.toString();
     }
     
-    Element generateIconUrl(){
-        Element result = new Element("IconUrl");
-        result.appendChild(ICONURL);
-        return result;
+    private String generateIconUrl(){
+        StringBuilder result = new StringBuilder(60);
+        result.append(getIndent(2));
+        result.append("<IconUrl>");
+        result.append(ICONURL);
+        result.append("</IconUrl>\n");
+        return result.toString();
     }
     
     
     //TODO what a package that has references from multiple databases
-    Element generateObjectSelector(DryadPackage pkg){
-        Element result = new Element("ObjectSelector");
-        result.appendChild(generateDatabase(pkg));
-        result.appendChild(generateObjectList(pkg));
-        return result;
+    private String generateObjectSelector(DryadPackage pkg){
+        StringBuilder result = new StringBuilder(300);
+        result.append(getIndent(2));
+        result.append("<ObjectSelector>\n");
+        result.append(generateDatabase(pkg));
+        result.append(generateObjectList(pkg));
+        result.append("</ObjectSelector>\n");
+        return result.toString();
     }
     
-    Element generateDatabase(DryadPackage pkg){
-        Element result = new Element("Database");
+    String generateDatabase(DryadPackage pkg){
+        StringBuilder result = new StringBuilder(30);
+        result.append(getIndent(3));
+        result.append("<Database>");
         if (TargetType.PUBLINKS == myType){
-            result.appendChild("PubMed");
+            result.append("PubMed");
         }
         else {
-            result.appendChild("TBD");
+            result.append("TBD");
         }
-        return result;
+        result.append("</Database>\n");
+        return result.toString();
     }
     
-    Element generateObjectList(DryadPackage pkg){
-        Element result = new Element("ObjectList");
+    private String generateObjectList(DryadPackage pkg){
+        StringBuilder result = new StringBuilder(120);
+        result.append(getIndent(3));
+        result.append("<ObjectList>\n");
         final Publication pub = pkg.getPub();
         if (pub != null){
             Set<String> pmids = pub.getPMIDs();
             for(String pmid : pmids){
-                result.appendChild("ObjId");  //TODO this isn't useful
+                String objIdElement = generateObjIdElement(pmid);
+                result.append(objIdElement);
             }
         }
-        return result;
+        result.append(getIndent(3));
+        result.append("</ObjectList>\n");
+        return result.toString();
     }
     
-    Element generateObjectUrl(DryadPackage pkg){
-        Element result = new Element("ObjectUrl");
-        result.appendChild(generateBase());
-        result.appendChild(generateRule());
-        return result;
-    }
     
-    Element generateBase(){
-        Element result = new Element("Base");
-        result.appendChild(DRYADBASE);
-        return result;
-    }
-    
-    Element generateRule(){
-        Element result = new Element("Rule");
-        result.appendChild(DRYADRULE);
-        return result;
+    private String generateObjIdElement(String dbId){
+        StringBuilder result = new StringBuilder(40);
+        result.append(getIndent(4));
+        result.append("<ObjId>");
+        result.append(dbId);
+        result.append("</ObjId>\n");
+        return result.toString();
     }
     
     
     
+    private String generateObjectUrl(DryadPackage pkg){
+        StringBuilder result = new StringBuilder(120);
+        result.append(getIndent(2));
+        result.append("<ObjectUrl>\n");
+        result.append(generateBase());
+        result.append(generateRule());
+        result.append(getIndent(2));
+        result.append("</ObjectUrl>\n");
+        return result.toString();
+    }
+    
+    private String generateBase(){
+        StringBuilder result = new StringBuilder(30);
+        result.append(getIndent(3));
+        result.append("<Base>");
+        result.append(DRYADBASE);
+        result.append("</Base>\n");
+        return result.toString();
+    }
+    
+    private String generateRule(){
+        StringBuilder result = new StringBuilder(30);
+        result.append(getIndent(3));
+        result.append("<Rule>");
+        result.append(DRYADRULE);
+        result.append("</Rule>\n");
+        return result.toString();
+    }
+    
+    final private static String indents = "     ";
+    private String getIndent(int indentCount){
+        if (indentCount == 0)
+            return "";
+        if (indentCount == 1)
+            return indents.substring(0,INDENTSIZE);
+        StringBuilder result = new StringBuilder(INDENTSIZE*indentCount);
+        for(int i = 0; i<indentCount; i++){
+            result.append(indents.substring(0,INDENTSIZE));
+        }
+        return result.toString();
+    }
     
     
     
